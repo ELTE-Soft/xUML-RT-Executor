8bed39ae986f2f5f000e4be7dfd900a9c34cfd7a
Gábor Ferenc Kovács <kovacsgabor@users.noreply.github.com>
2015-03-24 20:22:24 +0000

    m2m.logic.tests: complete 2015_q1_model added to test the SimpleM2MTranslator interface.

--- hu.eltesoft.modelexecution.m2m.logic.tests/src/hu/eltesoft/modelexecution/m2m/logic/tests/SimpleM2MTranslatorTests.java
+++ hu.eltesoft.modelexecution.m2m.logic.tests/src/hu/eltesoft/modelexecution/m2m/logic/tests/SimpleM2MTranslatorTests.java
@@ -9,37 +9,63 @@ import hu.eltesoft.modelexecution.m2m.logic.SimpleM2MTranslator;
 import org.eclipse.incquery.runtime.exception.IncQueryException;
 import org.junit.Test;
 
-public class SimpleM2MTranslatorTests extends IncQueryBasedTestCase{
+public class SimpleM2MTranslatorTests extends IncQueryBasedTestCase {
 
-	private static final String UML_TEST_MODEL_1_PATH = "resources/model1.uml"; 
-	
+	private static final String UML_TEST_SIMPLE_MODEL_PATH = "resources/simple_model.uml";
+	private static final String UML_TEST_2015_Q1_MODEL_PATH = "resources/2015_q1_model.uml";
+	private static final String[] UML_TEST_2015_Q1_MODEL_EXPECTED_FILES = {
+			"Class1", "Region1", "Method1", "Entry1", "Exit1", "Effect0", "Effect1", "Signal1", "SignalEvent1" };
+
 	private SimpleM2MTranslator translator;
 	private BasicTextChangesListener listener = new BasicTextChangesListener();
-	
+
 	private void initTranslator(String path) {
 		try {
-			translator = SimpleM2MTranslator.create(configureEngine(path), listener);
+			translator = SimpleM2MTranslator.create(configureEngine(path),
+					listener);
 		} catch (IncQueryException e) {
 			e.printStackTrace();
 		}
 	}
-	
+
 	@Test
-	public void testingOnModel1() {
-		initTranslator(UML_TEST_MODEL_1_PATH);
+	public void testingOnSimpleModel() {
+		initTranslator(UML_TEST_SIMPLE_MODEL_PATH);
 		assertEquals(0, listener.deletions.size());
 		assertEquals(0, listener.modifications.size());
-		
+
 		FileUpdateTaskQueue taskQueue = translator.fullBuild();
 		assertEquals(0, listener.deletions.size());
 		assertEquals(0, listener.modifications.size());
 		assertEquals(2, taskQueue.size());
-		
+
 		taskQueue.performAll();
 		assertEquals(0, listener.deletions.size());
 		assertEquals(2, listener.modifications.size());
-				
-		assertAsSets(new String[]{"A", "R1"}, listener.modifications.get(0), listener.modifications.get(1));
+
+		assertAsSets(new String[] { "A", "R1" }, listener.modifications.get(0),
+				listener.modifications.get(1));
 	}
-	
+
+	@Test
+	public void testingOn2015Q1Model() {
+		initTranslator(UML_TEST_2015_Q1_MODEL_PATH);
+
+		assertEquals(0, listener.deletions.size());
+		assertEquals(0, listener.modifications.size());
+
+		FileUpdateTaskQueue taskQueue = translator.fullBuild();
+		assertEquals(0, listener.deletions.size());
+		assertEquals(0, listener.modifications.size());
+		assertEquals(UML_TEST_2015_Q1_MODEL_EXPECTED_FILES.length, taskQueue.size());
+
+		taskQueue.performAll();
+		assertEquals(0, listener.deletions.size());
+		assertEquals(UML_TEST_2015_Q1_MODEL_EXPECTED_FILES.length,
+				listener.modifications.size());
+
+		assertAsSets(UML_TEST_2015_Q1_MODEL_EXPECTED_FILES,
+				listener.modifications.toArray(new String[0]));
+	}
+
 }
