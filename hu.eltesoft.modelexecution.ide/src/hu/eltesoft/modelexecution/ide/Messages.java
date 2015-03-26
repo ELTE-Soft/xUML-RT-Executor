1ac47fdfc2e610386a890c31b077c01f56abded1
Boldizsár Németh <nboldi@users.noreply.github.com>
2015-03-26 10:04:16 +0000

    merged ide branch into trunk

---
+++ hu.eltesoft.modelexecution.ide/src/hu/eltesoft/modelexecution/ide/Messages.java
@@ -0,0 +1,48 @@
+package hu.eltesoft.modelexecution.ide;
+
+import org.eclipse.osgi.util.NLS;
+
+public class Messages extends NLS {
+	private static final String BUNDLE_NAME = "hu.eltesoft.modelexecution.ide.messages"; //$NON-NLS-1$
+	public static String ExecutableModelProjectWizard_create_new_project_description;
+	public static String ExecutableModelProjectWizard_create_new_project_error_text;
+	public static String ExecutableModelProjectWizard_create_new_project_error_title;
+	public static String ExecutableModelProjectWizard_create_new_project_name;
+	public static String ExecutableModelProjectWizard_create_new_project_title;
+	public static String ExecutableModelPropertiesPage_project_properties_folder_for_generated_sources_label;
+	public static String LaunchConfigurationMainTab_launch_config_main_tab_caption;
+	public static String LaunchConfigurationMainTab_select_class_button_text;
+	public static String LaunchConfigurationMainTab_select_class_dialog_title;
+	public static String LaunchConfigurationMainTab_select_class_group_caption;
+	public static String LaunchConfigurationMainTab_select_class_tooltip;
+	public static String LaunchConfigurationMainTab_select_feed_button_text;
+	public static String LaunchConfigurationMainTab_select_feed_dialog_title;
+	public static String LaunchConfigurationMainTab_select_feed_group_caption;
+	public static String LaunchConfigurationMainTab_select_feed_tooltip;
+	public static String LaunchConfigurationMainTab_select_model_button_text;
+	public static String LaunchConfigurationMainTab_select_model_dialog_title;
+	public static String LaunchConfigurationMainTab_select_model_group_caption;
+	public static String LaunchConfigurationMainTab_select_model_tooltip;
+	public static String LaunchConfigurationTracingLoggingTab_enable_logging_label;
+	public static String LaunchConfigurationTracingLoggingTab_enable_tracing_label;
+	public static String LaunchConfigurationTracingLoggingTab_folder_for_tracefiles_button_text;
+	public static String LaunchConfigurationTracingLoggingTab_folder_for_tracefiles_dialog_title;
+	public static String LaunchConfigurationTracingLoggingTab_folder_for_tracefiles_label;
+	public static String LaunchConfigurationTracingLoggingTab_logging_group_caption;
+	public static String LaunchConfigurationTracingLoggingTab_trace_replay_button_label;
+	public static String LaunchConfigurationTracingLoggingTab_trace_replay_folder_dialog_title;
+	public static String LaunchConfigurationTracingLoggingTab_trace_replay_folder_for_tracefiles;
+	public static String LaunchConfigurationTracingLoggingTab_trace_replay_group_caption;
+	public static String LaunchConfigurationTracingLoggingTab_trace_replay_label;
+	public static String LaunchConfigurationTracingLoggingTab_tracing_and_logging_tab_name;
+	public static String LaunchConfigurationTracingLoggingTab_tracing_config_data_error_description;
+	public static String LaunchConfigurationTracingLoggingTab_tracing_config_data_error_title;
+	public static String LaunchConfigurationTracingLoggingTab_tracing_group_caption;
+	static {
+		// initialize resource bundle
+		NLS.initializeMessages(BUNDLE_NAME, Messages.class);
+	}
+
+	private Messages() {
+	}
+}
