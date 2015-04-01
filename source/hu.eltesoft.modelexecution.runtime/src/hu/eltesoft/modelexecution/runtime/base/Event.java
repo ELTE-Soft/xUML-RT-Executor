3b976dd8bdf076fa7e62c87b8bf324d18ce5921b
Karácsony Máté <k_mate@inf.elte.hu>
2015-04-01 20:05:46 +0000

    Removing broken release tag

--- source/hu.eltesoft.modelexecution.runtime/src/hu/eltesoft/modelexecution/runtime/base/Event.java
+++
@@ -1,9 +0,0 @@
-package hu.eltesoft.modelexecution.runtime.base;
-
-/**
- * A base class for UML events in the system. Events with the same source
- * (signal, operation, timer) are considered to be equal.
- */
-public abstract class Event {
-
-}
--- source/hu.eltesoft.modelexecution.runtime/src/hu/eltesoft/modelexecution/runtime/base/SignalEvent.java
+++
@@ -1,9 +0,0 @@
-package hu.eltesoft.modelexecution.runtime.base;
-
-/**
- * A base class for events that carry a signal. We do not distinguish between
- * SignalEvents carrying the same signal.
- */
-public abstract class SignalEvent extends Event {
-
-}
