385d5fd706f47dac814dcb73e47266ef2907c20b
Boldizsár Németh <nboldi@users.noreply.github.com>
2015-05-29 18:12:50 +0000

    changed instance registry to use a composite key.

--- plugins/hu.eltesoft.modelexecution.runtime/src/hu/eltesoft/modelexecution/runtime/InstanceRegistry.java
+++ plugins/hu.eltesoft.modelexecution.runtime/src/hu/eltesoft/modelexecution/runtime/InstanceRegistry.java
@@ -4,6 +4,7 @@ import hu.eltesoft.modelexecution.runtime.base.ClassWithState;
 
 import java.util.HashMap;
 import java.util.Map;
+import java.util.Objects;
 
 /**
  * Stores references for objects that can receive events. These events can come
@@ -13,9 +14,9 @@ import java.util.Map;
  * This class is a strictly loaded singleton.
  */
 public final class InstanceRegistry {
-	// if multiple runtimes are executed simultaneously, the mapping should be
-	// partitioned by Runtime
-	private Map<Class<?>, Map<Integer, ClassWithState>> instanceRegistry = new HashMap<>();
+	// If multiple runtimes are executed simultaneously, the mapping should be
+	// partitioned by Runtime and synchronized.
+	private Map<InstanceKey, ClassWithState> instanceRegistry = new HashMap<>();
 
 	private static final InstanceRegistry INSTANCE = new InstanceRegistry();
 
@@ -26,9 +27,7 @@ public final class InstanceRegistry {
 	 * Looks up an instance of the given class.
 	 */
 	public ClassWithState getInstance(Class<?> targetClass, int instanceID) {
-		Map<Integer, ClassWithState> classRegistry = instanceRegistry
-				.get(targetClass);
-		return classRegistry == null ? null : classRegistry.get(instanceID);
+		return instanceRegistry.get(new InstanceKey(targetClass, instanceID));
 	}
 
 	/**
@@ -36,11 +35,7 @@ public final class InstanceRegistry {
 	 * instance is registered, it cannot be garbage-collected.
 	 */
 	public void registerInstance(ClassWithState instance) {
-		Class<? extends ClassWithState> cls = instance.getClass();
-		if (!instanceRegistry.containsKey(cls)) {
-			instanceRegistry.put(cls, new HashMap<>());
-		}
-		instanceRegistry.get(cls).put(instance.getInstanceID(), instance);
+		instanceRegistry.put(new InstanceKey(instance), instance);
 	}
 
 	/**
@@ -49,8 +44,7 @@ public final class InstanceRegistry {
 	 * message.
 	 */
 	public void unregisterInstance(ClassWithState instance) {
-		instanceRegistry.get(instance.getClass())
-				.remove(instance.getInstanceID());
+		instanceRegistry.remove(new InstanceKey(instance));
 	}
 
 	/**
@@ -58,6 +52,36 @@ public final class InstanceRegistry {
 	 */
 	public static InstanceRegistry getInstanceRegistry() {
 		return INSTANCE;
+	}
+
+	private static final class InstanceKey {
+		public InstanceKey(ClassWithState instance) {
+			this.klass = instance.getClass();
+			this.instanceID = instance.getInstanceID();
+		}
+
+		public InstanceKey(Class<?> klass, int instanceID) {
+			this.klass = klass;
+			this.instanceID = instanceID;
+		}
+
+		private Class<?> klass;
+		private int instanceID;
+
+		@Override
+		public int hashCode() {
+			return Objects.hash(klass, instanceID);
+		}
+
+		@Override
+		public boolean equals(Object obj) {
+			if (obj == null || !(obj instanceof InstanceKey)) {
+				return false;
+			}
+			InstanceKey other = (InstanceKey) obj;
+			return instanceID == other.instanceID && klass.equals(other.klass);
+		}
+
 	}
 
 }
