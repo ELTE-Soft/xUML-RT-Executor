554af82e9c8f66a4c56daed33af6f31f78f130da
Boldizsár Németh <nboldi@users.noreply.github.com>
2015-08-22 10:14:47 +0000

    Show only the selected stack frame

---
+++ plugins/hu.eltesoft.modelexecution.ide/src/hu/eltesoft/modelexecution/ide/debug/DebugModelPresentation.java
@@ -0,0 +1,33 @@
+package hu.eltesoft.modelexecution.ide.debug;
+
+import org.eclipse.debug.core.model.IStackFrame;
+import org.eclipse.debug.ui.AbstractDebugView;
+import org.eclipse.jface.viewers.TreeSelection;
+import org.eclipse.papyrus.moka.ui.presentation.AnimationUtils;
+import org.eclipse.papyrus.moka.ui.presentation.MokaDebugModelPresentation;
+import org.eclipse.ui.IEditorPart;
+import org.eclipse.ui.PlatformUI;
+
+public class DebugModelPresentation extends MokaDebugModelPresentation {
+
+	private static final String DEBUG_VIEW_ID = "org.eclipse.debug.ui.DebugView";
+
+	@Override
+	public boolean addAnnotations(IEditorPart editorPart, IStackFrame frame) {
+		AnimationUtils.getInstance().removeSuspendedMarker(frame.getThread());
+		AbstractDebugView debugView = (AbstractDebugView) PlatformUI.getWorkbench().getActiveWorkbenchWindow()
+				.getActivePage().findView(DEBUG_VIEW_ID);
+		if (debugView == null) {
+			// debug view is not open
+			return true;
+		}
+		TreeSelection selection = (TreeSelection) debugView.getViewer().getSelection();
+		if (frame.equals(selection.getFirstElement())) {
+			return super.addAnnotations(editorPart, frame);
+		} else {
+			// not the selected stack frame
+			return true;
+		}
+	}
+
+}
