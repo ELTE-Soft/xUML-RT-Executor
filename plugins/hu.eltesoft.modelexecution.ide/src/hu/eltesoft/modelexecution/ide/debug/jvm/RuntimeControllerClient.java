8a549ccf05021238f5e936bf5ca7bedf97496b99
Karácsony Máté <k_mate@inf.elte.hu>
2015-09-05 15:47:25 +0000

    debugModel: fixing controller client in debugger

--- plugins/hu.eltesoft.modelexecution.ide/src/hu/eltesoft/modelexecution/ide/debug/jvm/RuntimeControllerClient.java
+++ plugins/hu.eltesoft.modelexecution.ide/src/hu/eltesoft/modelexecution/ide/debug/jvm/RuntimeControllerClient.java
@@ -26,13 +26,13 @@ import hu.eltesoft.modelexecution.runtime.RuntimeControllerServer;
 public class RuntimeControllerClient {
 
 	private ServerSocket server;
-	private Socket socket;
-	private Writer writer;
-	private BufferedReader reader;
 
-	private CountDownLatch ready = new CountDownLatch(1);
+	private volatile Socket socket;
+	private volatile Writer writer;
 
-	private List<StateMachnineInstanceListener> reactiveClassListeners = new LinkedList<>();
+	private final CountDownLatch ready = new CountDownLatch(1);
+
+	private final List<StateMachnineInstanceListener> reactiveClassListeners = new LinkedList<>();
 
 	public RuntimeControllerClient(ILaunchConfiguration launchConfig) {
 		try {
@@ -51,7 +51,7 @@ public class RuntimeControllerClient {
 				try {
 					socket = server.accept();
 					writer = new OutputStreamWriter(socket.getOutputStream());
-					reader = new BufferedReader(new InputStreamReader(socket.getInputStream()));
+					BufferedReader reader = new BufferedReader(new InputStreamReader(socket.getInputStream()));
 					ready.countDown();
 					String line;
 					while ((line = reader.readLine()) != null) {
@@ -95,7 +95,7 @@ public class RuntimeControllerClient {
 	public boolean terminate() {
 		return sendCommand(RuntimeControllerServer.COMMAND_TERMINATE);
 	}
-	
+
 	/**
 	 * Instructs the runtime to start the engine.
 	 */
