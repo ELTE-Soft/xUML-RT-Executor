8066c39ab061e403d2af3158923740aca1f84c4a
Boldizsár Németh <nboldi@users.noreply.github.com>
2015-09-30 11:57:23 +0000

    hotfix for children update exceptions of late updates

--- plugins/hu.eltesoft.modelexecution.ide.debug/src/hu/eltesoft/modelexecution/ide/debug/model/utils/CombiningContentProvider.java
+++ plugins/hu.eltesoft.modelexecution.ide.debug/src/hu/eltesoft/modelexecution/ide/debug/model/utils/CombiningContentProvider.java
@@ -9,6 +9,7 @@ import java.util.function.Function;
 
 import org.eclipse.core.runtime.CoreException;
 import org.eclipse.debug.internal.ui.model.elements.ElementContentProvider;
+import org.eclipse.debug.internal.ui.viewers.model.provisional.IChildrenUpdate;
 import org.eclipse.debug.internal.ui.viewers.model.provisional.IPresentationContext;
 import org.eclipse.debug.internal.ui.viewers.model.provisional.IViewerUpdate;
 
@@ -60,6 +61,20 @@ public class CombiningContentProvider<T> extends ElementContentProvider {
 		return ret;
 	}
 
+	@Override
+	protected void retrieveChildren(IChildrenUpdate update) {
+		// TODO: this concurrency issue may be inspected deeper, and solved more nicely
+		try {
+			// cannot perform an update when the number of children is changed since
+			if (update.getLength() != getChildCount(update.getElement(), update.getPresentationContext(), null)) {
+				update.cancel();
+			}
+		} catch (CoreException e) {
+			update.cancel();
+		}
+		super.retrieveChildren(update);
+	}
+	
 	@Override
 	protected boolean supportsContextId(String id) {
 		return childrenAccess.containsKey(id);
