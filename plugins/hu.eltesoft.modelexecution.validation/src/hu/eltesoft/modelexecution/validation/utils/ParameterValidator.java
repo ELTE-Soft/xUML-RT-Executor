0f05cfca64a8906e2e6bc2695d94ce598be9cd21
Boldizsár Németh <nboldi@users.noreply.github.com>
2015-10-06 15:49:43 +0000

    fixed a problem with the validation of parameter lists

--- plugins/hu.eltesoft.modelexecution.validation/src/hu/eltesoft/modelexecution/validation/utils/ParameterValidator.java
+++ plugins/hu.eltesoft.modelexecution.validation/src/hu/eltesoft/modelexecution/validation/utils/ParameterValidator.java
@@ -3,11 +3,14 @@ package hu.eltesoft.modelexecution.validation.utils;
 import java.util.Collection;
 import java.util.LinkedList;
 import java.util.List;
+import java.util.Optional;
 
 import org.eclipse.incquery.runtime.api.IPatternMatch;
 import org.eclipse.uml2.uml.BehavioralFeature;
 import org.eclipse.uml2.uml.Element;
 import org.eclipse.uml2.uml.Parameter;
+import org.eclipse.uml2.uml.ParameterDirectionKind;
+import org.eclipse.uml2.uml.Property;
 
 public class ParameterValidator extends BaseValidator {
 
@@ -20,41 +23,67 @@ public class ParameterValidator extends BaseValidator {
 		if (!super.check(match)) {
 			return false;
 		}
-		List<Parameter> parameters1 = getParameters((Element) match.get(0));
-		List<Parameter> parameters2 = getParameters((Element) match.get(1));
-		if (parameters1.size() != parameters2.size()) {
-			return true;
-		}
-		for (int i = 0; i < parameters1.size(); i++) {
-			Parameter param1 = parameters1.get(i);
-			Parameter param2 = parameters2.get(i);
-			if (!param1.getType().equals(param2.getType())) {
-				return true;
-			}
-			if (!param1.getDirection().equals(param2.getDirection())) {
-				return true;
-			}
-			if (param1.getLower() != param2.getLower()) {
-				return true;
-			}
-			if (param1.getUpper() != param2.getUpper()) {
-				return true;
-			}
-		}
-		return false;
+		List<DataElement> parameters1 = getParameters((Element) match.get(0));
+		List<DataElement> parameters2 = getParameters((Element) match.get(1));
+		return !parameters1.equals(parameters2);
 	}
-	
-	List<Parameter> getParameters(Element element) {
+
+	List<DataElement> getParameters(Element element) {
 		if (element instanceof BehavioralFeature) {
-			return ((BehavioralFeature) element).getOwnedParameters();
+			return createDataElems(((BehavioralFeature) element).getOwnedParameters());
 		}
-		List<Parameter> ret = new LinkedList<>();
-		for (Element subElem : element.allOwnedElements()) {
-			if (subElem instanceof Parameter) {
-				ret.add((Parameter) subElem);
+		return createDataElems(element.getOwnedElements());
+	}
+
+	private List<DataElement> createDataElems(List<? extends Element> datas) {
+		List<DataElement> ret = new LinkedList<>();
+		for (Element data : datas) {
+			DataElement create = DataElement.create(data);
+			if (create != null) {
+				ret.add(create);
 			}
 		}
 		return ret;
+	}
+
+	private static class DataElement {
+
+		public static DataElement create(Element element) {
+			if (element instanceof Parameter) {
+				Parameter parameter = (Parameter) element;
+				return new DataElement(parameter.getName(), Optional.of(parameter.getDirection()), parameter.getLower(),
+						parameter.getUpper());
+			} else if (element instanceof Property) {
+				Property property = (Property) element;
+				return new DataElement(property.getName(), Optional.empty(), property.getLower(), property.getUpper());
+			} else {
+				return null;
+			}
+		}
+
+		private DataElement(String name, Optional<ParameterDirectionKind> direction, int lowerBound, int upperBound) {
+			this.name = name;
+			this.direction = direction;
+			this.lowerBound = lowerBound;
+			this.upperBound = upperBound;
+		}
+
+		private String name;
+		private Optional<ParameterDirectionKind> direction;
+		private int lowerBound;
+		private int upperBound;
+
+		@Override
+		public boolean equals(Object obj) {
+			if (obj == null || !(obj instanceof DataElement)) {
+				return false;
+			}
+			DataElement other = (DataElement) obj;
+			return name.equals(other.name) && lowerBound == other.lowerBound && upperBound == other.upperBound
+					&& direction.orElse(ParameterDirectionKind.IN_LITERAL)
+							.equals(other.direction.orElse(ParameterDirectionKind.IN_LITERAL));
+		}
+
 	}
 
 }
