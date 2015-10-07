e93bc809d735b7fade174907c5e28664b2d6270d
Karácsony Máté <k_mate@inf.elte.hu>
2015-10-07 15:43:29 +0000

    cli-testing: labels for methods testing invalid arguments

--- tests/hu.eltesoft.modelexecution.cli.tests/src/hu/eltesoft/modelexecution/cli/InvalidArgumentTests.java
+++ tests/hu.eltesoft.modelexecution.cli.tests/src/hu/eltesoft/modelexecution/cli/InvalidArgumentTests.java
@@ -33,13 +33,13 @@ public class InvalidArgumentTests {
 		assertFalse(output.getValue().isEmpty());
 	}
 
-	@Parameters
+	@Parameters(name = "Arguments: ''{0}''")
 	public static Collection<Object[]> getParameters() {
 		return Arrays
 				.asList("", "-s", "-s notExistingFile", "-wtr someArg", "-rtr someArg", "-rtr someArg -s someArg",
 						"-r someArg", "-l someArg", "-l invalidArg -e someArg", "-e someArg",
 						"dangling1 -e someArg someArg2 dangling2", "-r invalidDir -e someArg someArg2",
-						"-rtr invalidDir -e someArg someArg2", "-rtr invalidDir -e someArg someArg2", "-s invalidFile",
+						"-rtr invalidDir -e someArg someArg2", "-s invalidFile",
 						"-l invalidValue -e someArg someArg2", "-unknownArgument")
 				.stream().map(args -> new Object[] { args }).collect(Collectors.toList());
 	}
